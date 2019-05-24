3ee70df35bf249aa53466440cd72381b271e3452
fll121114 <893206766@qq.com>
2019-05-24 15:34:43 +0800

    提交本地代码

---
+++ myself/springboot_quick2/src/main/java/com/maimaicc/SpringbootQuick2Application.java
@@ -0,0 +1,15 @@
+package com.maimaicc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuick2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootQuick2Application.class, args);
+	}
+
+
+}
